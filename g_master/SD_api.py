07d78506207ebfc156fe79ebd2784bb502563d14
Dim <cibershaman@gmail.com>
2023-07-20 21:41:44 +0300

    Create SD_api.py

---
+++ g_master/SD_api.py
@@ -0,0 +1,18 @@
+import requests, base64
+
+def gen_image(prompt,extra={}):
+  '''
+  https://github.com/AUTOMATIC1111/stable-diffusion-webui
+  must be on http://devbox:7860, or change requests URL
+  '''
+    payload = { "prompt": prompt, "steps": 20 }
+    payload.update(extra)
+    response = requests.post(url=f'http://devbox:7860/sdapi/v1/txt2img', json=payload)
+    if response.status_code != 200:
+        print( "Non-200 response: " + str(response.text))
+        #bot.send_message(chat_id, "Non-200 response: " + str(response.text))
+        return
+    r = response.json()
+    print(r['info'])
+    images = [base64.b64decode(i.split(",",1)[0]) for i in r['images']]
+    return images[0]
